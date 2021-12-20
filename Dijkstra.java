16219680c2a04b3ebe3c557f92f1a6617b7785b4
CPCS324 <ralsaeed0010@stu.kau.edu.sa>
2021-12-21 01:51:11 +0300

    Dijkstra.java was updated

--- Dijkstra.java
+++ Dijkstra.java
@@ -16,8 +16,23 @@ public class Dijkstra {
 
     // global variables declaration 
     
-    // array of vertices
+    // array of vertices for graph 1
     static String[] vertices = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+    
+    // array of vertices for graph 2
+    //static String[] vertices = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"};
+
+    // array of vertices for graph 3
+    //static String[] vertices={"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"};
+    
+    // array of vertices for graph 4
+    //static String[] vertices={"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"};
+    
+    // array of vertices for graph 5
+    //static String[] vertices={"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A1", "B1", "C1", "D1"};
+    
+    //to save the tree
+    static String tree = "";
     //to save the visited nodes labels
     static String visited = "";
     //Edges from tybe ArrayList<edge>
@@ -62,6 +77,11 @@ public class Dijkstra {
             
             // Update the distance between neighbouring vertex and source vertex
             int Newdis = findMinDis(distance, isvisited);
+            
+            if (Newdis == -1) { //if vertex isolated
+                break;
+            }
+
             isvisited[Newdis] = true;
             
             
@@ -78,11 +98,13 @@ public class Dijkstra {
             }
             
             // printing tree vertex 
-            System.out.println("\nTree Vertices (VT)");
-            System.out.println(vertices[Newdis] + "(" + (vertices[source].equals(vertices[Newdis])?"-":vertices[Edges.get(Newdis).source]) + "," + distance[Newdis] + ")\n");
+            System.out.println("\nTree Vertices (VT)\n");
             
+            tree += vertices[Newdis] + "(" + (vertices[source].equals(vertices[Newdis]) ? "-" : vertices[Edges.get(Newdis).source]) + "," + distance[Newdis] + ")\n";
+            System.out.println(tree);
+
             // printing remaining vertices
-            System.out.println("Remaining Vertices (V-VT)");
+            System.out.println("Remaining Vertices (V-VT)\n");
             
             for (int j = 0; j < matrixLen; j++) {
                 
@@ -98,9 +120,13 @@ public class Dijkstra {
         System.out.println("\nShortest distances from "+ vertices[source] +" to all other vertices are:\n");
         for (int i = 0; i < distance.length; i++) {
             
-            if(!vertices[source].equals( vertices[i]))
-            System.out.println(String.format("   >> Distance from %s to %s is %d km", vertices[source], vertices[i], distance[i]));
-        
+            if(!vertices[source].equals( vertices[i])){
+                if (distance[i] == Integer.MAX_VALUE) {
+                    System.out.println(String.format("   >> There is no Distance from %s to %s ", vertices[source], vertices[i]));
+                } else {
+                    System.out.println(String.format("   >> Distance from %s to %s is %d ", vertices[source], vertices[i], distance[i]));
+                }
+            }
         }
         System.out.println();//New Line
         
@@ -145,7 +171,7 @@ public class Dijkstra {
      */
     public static void main(String[] args) {
         
-        // The given matrix
+        // The First graph (The given matrix - 10 Vertices)
                                  //  A   B   C    D    E   F   G    H    I    J 
         int matrix[][] = new int[][]{{0, 10, INF, INF, INF, 5, INF, INF, INF, INF},    // A
                                     {INF, 0, 3, INF, 3, INF, INF, INF, INF, INF},     // B
@@ -158,6 +184,130 @@ public class Dijkstra {
                                     {INF, INF, INF, INF, INF, INF, INF, INF, 0, INF}, // I
                                     {INF, 6, INF, INF, INF, INF, 8, INF, INF, 0},     // J    
                                     };
+        
+        //--------------------------------------------------------------------------
+        
+        /*  
+        // The second graph (2 - 15 Vertices)
+        
+                        //   A   B    C    D    E    F    G    H    I    G    K    L    M    N    O
+        int matrix[][] = {{  0, INF, INF, INF, INF,   3, INF, INF, INF, INF, INF, INF, INF, INF,   6}, // A
+                          {  8,   0, INF, INF, INF, INF, INF, INF, INF, INF,   2, INF,   5, INF,   3}, // B
+                          {INF, INF,   0, INF, INF, INF, INF,   9, INF, INF, INF, INF,   7,   4, INF}, // C
+                          {  2, INF, INF,   0, INF,   1, INF, INF, INF,   3, INF, INF, INF,   7,   9}, // D
+                          {INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // E
+                          {INF, INF, INF, INF, INF,   0, INF,   9, INF, INF, INF, INF, INF, INF,   2}, // F
+                          {INF, INF,   3, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF}, // G
+                          {INF, INF, INF, INF, INF, INF, INF,   0,   4, INF, INF,   2, INF, INF, INF}, // H
+                          {  1, INF, INF, INF, INF, INF, INF,   6,   0, INF, INF, INF, INF,   2, INF}, // I
+                          {INF, INF, INF, INF, INF,   9, INF, INF, INF,   0, INF,   1, INF, INF,   4}, // J
+                          {INF, INF, INF, INF, INF, INF,   6,   2, INF, INF,   0, INF, INF, INF, INF}, // K
+                          {INF, INF, INF,  10,   3, INF, INF, INF,   5, INF, INF,   0, INF,   1,   2}, // L
+                          {INF, INF, INF,   9, INF, INF, INF,   1,   4, INF,   7, INF,   0, INF, INF}, // M
+                          {INF, INF, INF,   3, INF, INF, INF, INF,   8,   2, INF, INF, INF,   0, INF}, // N
+                          {  4, INF,   8, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0}, // O
+                         }; */
+        
+        //--------------------------------------------------------------------------
+        
+        /*
+        // The third graph (3 - 20 Vertices)
+        
+                        //   A   B    C    D    E    F    G    H    I    G    K    L    M    N    O    P    Q    R    S    T
+        int matrix[][] = {{  0, INF, INF, INF, INF, INF, INF, INF,   1, INF, INF, INF, INF, INF, INF, INF, INF,   4, INF, INF}, // A
+                          {INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF,   3, INF, INF,   8,   2, INF, INF, INF, INF}, // B
+                          {INF, INF,   0, INF, INF,   1, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF, INF}, // C
+                          {INF, INF, INF,   0, INF, INF, INF, INF,   5, INF, INF,   9, INF, INF, INF, INF, INF, INF, INF, INF}, // D
+                          {INF, INF, INF, INF,   0, INF, INF, INF,   1, INF, INF, INF,   2,  10, INF, INF, INF, INF, INF, INF}, // E
+                          {  1, INF, INF, INF, INF,   0, INF,   4, INF, INF, INF, INF,   1, INF, INF, INF, INF, INF, INF, INF}, // F
+                          {INF, INF, INF, INF, INF, INF,   0,   7, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   5, INF}, // G
+                          {INF, INF, INF, INF,   3,   1, INF,   0, INF, INF, INF, INF,   9, INF, INF, INF, INF,   1, INF, INF}, // H
+                          {INF, INF,   7, INF, INF, INF, INF, INF,   0, INF, INF, INF,   1, INF, INF, INF, INF, INF, INF, INF}, // I
+                          {INF, INF, INF,   6,   1, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // G
+                          {INF, INF, INF, INF, INF, INF,   5, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // K
+                          {INF, INF, INF, INF,   2, INF, INF, INF, INF, INF,   9,   0, INF, INF, INF,   4,   2,   1, INF, INF}, // L
+                          {INF, INF,   4, INF, INF, INF, INF, INF, INF, INF,   3, INF,   0, INF, INF, INF, INF, INF, INF, INF}, // M
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF}, // N
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF}, // O
+                          {INF, INF,   7, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF}, // P
+                          {INF, INF, INF, INF, INF, INF, INF,   1, INF, INF, INF, INF, INF, INF,  10, INF,   0, INF,   1, INF}, // Q
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   3,   0, INF, INF}, // R
+                          {INF, INF, INF, INF, INF, INF, INF,  10, INF, INF,   4, INF, INF, INF, INF, INF, INF, INF,   0, INF}, // S
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   1,   6, INF, INF, INF, INF,   9,   0}, // T
+                         }; */
+        
+        //--------------------------------------------------------------------------
+
+        /*
+        // The fourth graph (4 - 25 Vertices)
+        
+                        //   A   B    C    D    E    F    G    H    I    G    K    L    M    N    O    P    Q    R    S    T    U    V    W    X    Y
+        int matrix[][] = {{  0, INF, INF, INF, INF,   4, INF, INF,   2,   9, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // A
+                          {INF,   0, INF, INF, INF, INF, INF,   2, INF, INF,   5, INF, INF, INF, INF, INF, INF, INF,   3, INF, INF, INF, INF, INF, INF}, // B
+                          {INF,  10,   0, INF, INF, INF, INF,   3, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   8, INF, INF, INF, INF}, // C
+                          {INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // D
+                          {INF, INF, INF,   9,   0, INF,   5, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   1, INF, INF}, // E
+                          {INF, INF,   7, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF, INF}, // F
+                          {INF, INF, INF, INF, INF, INF,   0, INF, INF,   6, INF, INF,   3, INF,   1, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // G
+                          {INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // H
+                          {INF, INF, INF, INF, INF, INF, INF,  10,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // I
+                          {INF, INF, INF,   2, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF,   8, INF, INF,   1, INF, INF, INF}, // J
+                          {INF, INF, INF, INF,   4, INF, INF, INF, INF, INF,   0, INF, INF,   5, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // K
+                          {INF,   8, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF,   7, INF, INF, INF, INF,   7, INF}, // L
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // M
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF,  10, INF, INF, INF, INF}, // N
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   1, INF,   2,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // O
+                          {INF,   8, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   4,   0, INF, INF, INF, INF, INF, INF,   2, INF, INF}, // P
+                          {INF, INF,   1, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   7, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF}, // Q
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF,   3, INF, INF, INF, INF}, // R
+                          {INF, INF, INF, INF, INF,  10, INF, INF, INF, INF, INF, INF, INF,   4, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF}, // S
+                          {INF, INF, INF, INF, INF, INF, INF,   5, INF,   3, INF, INF, INF, INF, INF, INF,   8, INF, INF,   0, INF, INF, INF, INF, INF}, // T
+                          {INF, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF}, // U
+                          {  4,   2, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF}, // V
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   8, INF, INF, INF, INF,   0, INF, INF}, // W
+                          {INF,   7, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF}, // X
+                          {INF, INF, INF, INF, INF, INF, INF,   1, INF, INF, INF, INF, INF, INF, INF, INF, INF,   9, INF, INF, INF, INF, INF,   3,   0}, // Y
+                         }; */
+        
+        //--------------------------------------------------------------------------
+
+        /*
+        // The fifth graph (5 - 30 Vertices)
+        
+                        //   A   B    C    D    E    F    G    H    I    G    K    L    M    N    O    P    Q    R    S    T    U    V    W    X    Y    Z    A1   B1   C1   D1  
+        int matrix[][] = {{  0, INF, INF, INF, INF, INF, INF, INF, INF,   5, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // A
+                          {INF,   0, INF, INF, INF, INF,   3, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // B
+                          {INF,   7,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   9, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // C
+                          {INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // D
+                          {INF, INF, INF, INF,   0,   4, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // E
+                          {INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   1, INF, INF, INF, INF, INF}, // F
+                          {INF, INF, INF, INF, INF,   2,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // G
+                          {INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   3, INF,   2}, // H
+                          {  5, INF, INF, INF, INF,   6, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF,   1, INF, INF,   6, INF, INF, INF,   5, INF,   4, INF, INF, INF}, // I
+                          {INF, INF, INF, INF, INF, INF, INF,   8, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // J
+                          {INF, INF, INF,   2, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // K
+                          {INF, INF, INF, INF, INF, INF, INF, INF,   1, INF, INF,   0, INF, INF,   5, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   7, INF,   9, INF, INF}, // L
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // M
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   1,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // N
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF,   9, INF, INF, INF, INF}, // O
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF,   4, INF, INF, INF, INF, INF}, // P
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0,   7, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   5, INF}, // Q
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   3, INF, INF, INF, INF,   0, INF,   1, INF, INF, INF, INF,   8, INF, INF, INF, INF, INF}, // R
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF,   6, INF, INF, INF, INF,   1, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // S
+                          {INF, INF, INF, INF,   3,   8, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // T
+                          {INF,   8, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF, INF}, // U
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF,   7, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF, INF}, // V
+                          {INF, INF,   5, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF, INF, INF, INF, INF}, // W
+                          {INF, INF, INF, INF, INF, INF, INF, INF,   9, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   9,   0, INF, INF, INF, INF, INF, INF}, // X
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   7, INF,   0, INF, INF, INF, INF, INF}, // Y
+                          {INF, INF,   2, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   1, INF, INF, INF, INF, INF, INF,   3, INF, INF,   0, INF, INF, INF, INF}, // Z
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF, INF}, // A1
+                          {INF, INF,   3, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   7, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF, INF}, // B1
+                          {  9, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   0, INF}, // C1
+                          {INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF, INF,   2, INF, INF,   0}, // D1
+                        }; */
+        
+        //--------------------------------------------------------------------------
         
         // start sentence 
         System.out.println("------------------- WELCOME TO Dijkstra ALGORITHM -------------------\n\n");
